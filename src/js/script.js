122cf45627668712d88280beb4ca2830ac75a05c
Mayuu_ <40952685+samirmedani@users.noreply.github.com>
2022-06-04 00:02:08 +0200

    save

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,15 @@
-// Scrolling menu open and close
-const OpenNav = document.querySelector(".fa-solid");
-const NavCategory = document.querySelector(".NavCategory");
+// // Scrolling menu open and close
+// const OpenNav = document.querySelector(".fa-solid");
+// const NavCategory = document.querySelector(".NavCategory");
 
-OpenNav.addEventListener("click", function () {
-  NavCategory.classList.toggle("hidden");
-});
+// OpenNav.addEventListener("click", function () {
+//   NavCategory.classList.toggle("hidden");
+// });
 
-// College
-const college = document.querySelector(".college");
-const collegeSubmenu = document.querySelector(".college-submenu");
+// // College
+// const college = document.querySelector(".college");
+// const collegeSubmenu = document.querySelector(".college-submenu");
 
-college.addEventListener("click", function () {
-  collegeSubmenu.classList.toggle("hidden");
-});
+// college.addEventListener("click", function () {
+//   collegeSubmenu.classList.toggle("hidden");
+// });
